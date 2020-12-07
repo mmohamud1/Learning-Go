946539e83d855a20b753a38047f1f23d0aae1ca6
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-07 14:17:16 +0000

    Commit

--- src/github.com/mmohamud1/go_crash_course/02_vars/main.go
+++ src/github.com/mmohamud1/go_crash_course/02_vars/main.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+
 func main() {
 	// GO DATA TYPES
 	// string
@@ -17,12 +18,24 @@ func main() {
 	// Using var keyword
 	var name = "Mohamed"
 	var age = 24
-
+	var size float32 = 2.3
+	
 	// Using const 
-	var isCool = true
+	const isCool = true
+	
+	// shorthand
+	nameTwo := "Mo"
+	ageTwo := "32"
+	sizeTwo := 1.3
+
+	job, numOfYears := "develper", 1
 
 	// show name and age in terminal
-	fmt.Println(name, age, isCool)
+	fmt.Println(name, age, isCool, nameTwo, ageTwo, sizeTwo, job, numOfYears)
 	// get type of variable e.g string
 	fmt.Printf("%T\n", name)
+	fmt.Printf("%T\n", age)
+	fmt.Printf("%T\n", isCool)
+	fmt.Printf("%T\n", size)
+
 }
