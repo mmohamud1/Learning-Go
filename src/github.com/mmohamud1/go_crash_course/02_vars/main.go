ec55edd1ecddefc0d269a749e22a8671f1c69dd7
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-07 13:26:05 +0000

    Commit

--- src/github.com/mmohamud1/go_crash_course/02_vars/main.go
+++ src/github.com/mmohamud1/go_crash_course/02_vars/main.go
@@ -6,4 +6,11 @@ func main() {
 	// GO DATA TYPES
 	// string
 	// bool
+	// int
+	// int int8 int16 in32 int64
+	// uint uint8 uint16 uint32 uint 64 uintptr
+	// byte - alias for uint8
+	// rune - alias for int32
+	// float32 float64
+	// complex64 complex 128
 }
