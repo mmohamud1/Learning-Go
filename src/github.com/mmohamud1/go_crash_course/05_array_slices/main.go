b9699aaa2ad51c34cf35a06ba6513bb60a207d8f
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-09 12:49:15 +0000

    Commit

--- src/github.com/mmohamud1/go_crash_course/05_array_slices/main.go
+++ src/github.com/mmohamud1/go_crash_course/05_array_slices/main.go
@@ -3,14 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	// Arrays
+	// Arrays declare
 	var fruitArr [2]string
 
 	// Assign values
 	fruitArr[0] = "Apple"
 	fruitArr[1] = "Orange"
 
+	// Declare and assign
+	teamArr := [2] string{"Liverpool", "Arsenal"}
+
 	fmt.Println(fruitArr)
 	fmt.Println(fruitArr[1])
+	fmt.Println(teamArr)
 
 }
