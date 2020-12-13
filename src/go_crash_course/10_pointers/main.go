a166eaa5dd2caf3fbbb88889cf4e5838b46ef554
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-13 13:26:35 +0000

    Commit

--- src/go_crash_course/10_pointers/main.go
+++ src/go_crash_course/10_pointers/main.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	a := 5
+	b := &a
+
+	fmt.Println(a, b)
+	fmt.Printf("%T\n", b)
+
+	// Use * to read value from address
+	fmt.Println(*b)
+
+	// Change val with pointer
+	*b = 10
+	fmt.Println(a)
 }
