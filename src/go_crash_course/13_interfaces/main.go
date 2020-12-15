73df413d53c7b9b6d9128157b5ce741f9e8e81fb
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-15 13:58:08 +0000

    Commit

--- src/go_crash_course/13_interfaces/main.go
+++ src/go_crash_course/13_interfaces/main.go
@@ -3,6 +3,19 @@ package main
 import ("fmt"
 		"math")
 
+// Define interfaces
+type Shape interface {
+	area() float64
+}
+
+type Circle struct {
+	x, y, radius float64
+}
+
+type Rectangle struct {
+	width, height float64
+}
+
 func main() {
 	fmt.Println("Hello World")
 }
