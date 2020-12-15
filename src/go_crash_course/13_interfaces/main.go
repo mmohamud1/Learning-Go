104c2ae151169abd610717cd8a97c122d46f36f2
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-15 14:07:36 +0000

    Commit

--- src/go_crash_course/13_interfaces/main.go
+++ src/go_crash_course/13_interfaces/main.go
@@ -26,6 +26,16 @@ func (r Rectangle) area() float64 {
 	return r.width * r.height
 }
 
+// area of shape
+func getArea(s Shape) float64 {
+	return s.area()
+}
+
 func main() {
-	fmt.Println("Hello World")
+	circle := Circle{x: 0, y: 0, radius: 5}
+	rectangle := Rectangle{width: 10, height: 5}
+
+	fmt.Printf("Circle Area: %f\n", circle)
+	fmt.Printf("Rectangle Area: %f\n", rectangle)
+
 }
