acb4ead17b8bd45a6472c04466147cf4d2a8c4c7
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-15 14:02:26 +0000

    Commit

--- src/go_crash_course/13_interfaces/main.go
+++ src/go_crash_course/13_interfaces/main.go
@@ -16,6 +16,16 @@ type Rectangle struct {
 	width, height float64
 }
 
+// area of circle
+func (c Circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+// area of rectangcle
+func (r Rectangle) area() float64 {
+	return r.width * r.height
+}
+
 func main() {
 	fmt.Println("Hello World")
 }
